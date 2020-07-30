ae530d4a202efd50795994e4b432f0f532a2b065
张一 <2326962137@qq.com>
2020-07-30 12:44:27 +0800

    初始化项目

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'antd';
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">App</Button>
+    </div>
+  );
+}
+
+export default App;
